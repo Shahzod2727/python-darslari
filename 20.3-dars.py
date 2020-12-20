ade4710ad794c2784a6d748e35c8dfb6bab22b0b
Shahzod2727 <76176843+Shahzod2727@users.noreply.github.com>
2020-12-20 10:06:05 +0500

    yangilandi
    20-dars

---
+++ 20.3-dars.py
@@ -0,0 +1,54 @@
+#Uchta son qabul qilib, ulardan eng kattasini qaytaruvchi funksiya yozing
+
+#def uchta_son(A,B,C):
+ #   if (A>B)and(A>C):
+#        D=A
+#    elif (B>A)and(B>C):
+#        D=B
+#    elif(C>A)and(C>B):
+#        D=C
+
+#    return D
+
+#t = uchta_son(5,8,10)
+#print(t)
+
+#Foydalanuvchidan aylaning radiusini qabul qilib olib,
+# uning radiusini, diametrini, perimetri va yuzini lug'at ko'rinishida qaytaruvchi funksiya yozing
+
+#def aylana(r):
+#    R = {
+#        'r':r,
+#        'd':2*r,
+#        's':(3.14)*r*r
+#    }
+
+#    return R
+#a = aylana(5)
+#print(a)
+
+#Berilgan oraliqdagi tub sonlar ro'yxatini qaytaruvchi funksiya yozing
+#  (tub sonlar —faqat birga va o'ziga qoldiqsiz bo'linuvchi, 1 dan katta musbat sonlar)
+
+
+
+
+
+
+
+
+
+
+
+#Foydalanuvchidan son qabul qilib, shu son miqdoricha Fibonachchi ketma-ketligidagi sonlar ro'yxatni
+# qaytaruvchi funksiya yozing.  Ta’rif: Har bir hadi o’zidan oldingi ikkita hadning yig’indisiga teng bo’lgan
+# ketma-ketlik Fibonachchi ketma-ketligi deyiladi. Bunda boshlang’ish had ko’pincha 1 deb olinadi.  1, 1, 2, 3, 5, 8, 13, 21, 34, 55,...
+
+#F(n) = F(n-1) + F(n-2) va F(0) = 0, F(1) = 1
+
+
+# Function for nth Fibonacci number
+
+
+
+#This code is contributed by Saket Modi
